812704b322ec06a52e0e2f8867ffed578b0a882b
Henrique Naylor <henriquenl09@gmail.com>
2023-10-14 21:54:23 -0300

    added base server files

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response , Application, NextFunction } from 'express';
+import dotenv from 'dotenv';
+
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// log all requests
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(`${req.method} ${req.path} ${req.ip} ${new Date().toLocaleString()}`);
+    next();
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
