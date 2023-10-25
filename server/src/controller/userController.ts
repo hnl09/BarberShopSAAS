15cbf7cc6b327540e8335e394c1c0cbfaa3f44e0
Henrique Naylor Lopes <henriquenl09@gmail.com>
2023-10-25 15:05:55 -0300

    created model to User and signupUser route

--- server/src/controller/userController.ts
+++ server/src/controller/userController.ts
@@ -1,10 +1,29 @@
 import { Request, Response } from "express"
 import userModel from "../models/userModel"
+import bcrypt from "bcrypt"
 
 export const loginUser = async (req: Request, res: Response) => {
     res.json({ message: "Login user" })
 }
 
 export const signUpUser = async (req: Request, res: Response) => {
-    res.json({ message: "Signup user" })
+    const {email, password} = req.body
+
+    try {
+        const emailExists = await userModel.findOne({ email: email })
+        if (emailExists) {
+            res.status(400).send({ message: 'Email already exists'});
+            throw new Error('Email already exists');
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+      
+        const user = await userModel.create({ email, password: hash });
+
+        const userWithoutPassword = user.toObject();
+        delete userWithoutPassword.password;
+        res.json(userWithoutPassword);
+    } catch(error){
+        console.error(error)
+    }
 }
--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -1,15 +1,29 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 
 const Schema = mongoose.Schema
 
-export interface UserDocument extends mongoose.Document {
+export interface UserDocument extends Document {
     email: string;
     password: string;
+    firstName: string;
+    lastName: string;
+    barberShopName: string;
+    createdAt: Date;
+    updatedAt: Date;
+    isActive: boolean;
+    isVerified: boolean;
 }
 
-const userSchema = new Schema({
+const userSchema = new Schema<UserDocument>({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    firstName: {type: String},
+    lastName: {type: String},
+    barberShopName: {type: String},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now},
+    isActive: {type: Boolean, default: true},
+    isVerified: {type: Boolean, default: false}
 })
 
 const userModel = mongoose.model<UserDocument>('User', userSchema)
