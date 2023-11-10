45906edaa414526b51f3966a8a04118bfcbb3e3b
Henrique Naylor Lopes <henriquenl09@gmail.com>
2023-11-10 14:22:47 -0300

    removed message attribute on schema

--- server/src/models/messageModel.ts
+++ server/src/models/messageModel.ts
@@ -6,14 +6,12 @@ export interface MessageDocument extends Document {
     customer: Object;
     messageContent: string;
     timestamp: Date;
-    status: string;
 }
 
 const messageSchema = new mongoose.Schema<MessageDocument>({
     customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer' },
     messageContent: {type: String},
     timestamp: { type: Date, default: Date.now },
-    status: {type: String},
 });
 
 const messageModel = mongoose.model<MessageDocument>('Message', messageSchema)
